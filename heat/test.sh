4eab8548ddaed30caf9d824f0b8c977cf5467575
AlexandarAllakhverdieva <aallakhverdieva@mirantis.com>
2015-12-08 17:53:10 +0300

    small fix for run tests

--- heat/test.sh
+++ heat/test.sh
@@ -1,5 +1,5 @@
 CONTR_ID=$(fuel node | grep controller | head -1 | awk '{print$1}')
-scp test_heat.sh node-$CONTR_ID:~/
+scp heat_tests.py node-$CONTR_ID:~/
 scp -r Templates node-$CONTR_ID:~/
 
 ssh node-$CONTR_ID "source ~/openrc && nosetests heat_tests.py --with-xunit --xunit-file=heat_tests_report.xml"
