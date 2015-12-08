ee17e5fd9f29ea2777b917656f7be801cfdcf985
Alexander Koryagin <akoryag@mera.ru>
2015-12-08 12:09:03 +0300

    add xmllint to output for user

--- heat/test.sh
+++ heat/test.sh
@@ -4,4 +4,4 @@ scp heat_tests.py node-$CONTR_ID:~/
 
 ssh node-$CONTR_ID "source ~/openrc && nosetests heat_tests.py --with-xunit --xunit-file=heat_tests_report.xml"
 scp node-$CONTR_ID:~/heat_tests_report.xml ~/
-echo -e "For test results see:\n\t# less ~/heat_tests_report.xml"
+echo -e "For test results see:\n\t# less ~/heat_tests_report.xml\n\t# xmllint --format ~/heat_tests_report.xml"
