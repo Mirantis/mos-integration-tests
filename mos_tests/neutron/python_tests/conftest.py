bb8dcca9fd55dc654ded8192527051af88240dff
Georgy Dyuldin <g.dyuldin@gmail.com>
2015-12-17 14:24:01 +0300

    Add check to prevent revert env after skipped test

--- mos_tests/neutron/python_tests/conftest.py
+++ mos_tests/neutron/python_tests/conftest.py
@@ -25,6 +25,16 @@ from mos_tests.settings import KEYSTONE_PASS
 from mos_tests.settings import SSH_CREDENTIALS
 
 
+def pytest_runtest_makereport(item, call):
+    if call.excinfo is not None and call.excinfo.typename == 'Skipped':
+        setattr(item, 'env_destroyed', False)
+
+
+def pytest_runtest_teardown(item, nextitem):
+    if getattr(item, 'destroyed', True) and nextitem is not None:
+        setattr(nextitem, 'do_revert', True)
+
+
 @pytest.fixture
 def env_name(request):
     return request.config.getoption("--env")
@@ -36,10 +46,11 @@ def snapshot_name(request):
 
 
 @pytest.fixture
-def revert_snapshot(env_name, snapshot_name):
+def revert_snapshot(request, env_name, snapshot_name):
     """Revert Fuel devops snapshot before test"""
-    DevopsClient.revert_snapshot(env_name=env_name,
-                                 snapshot_name=snapshot_name)
+    if getattr(request.node, 'do_revert', True):
+        DevopsClient.revert_snapshot(env_name=env_name,
+                                     snapshot_name=snapshot_name)
 
 
 @pytest.fixture
@@ -48,8 +59,8 @@ def fuel_master_ip(request, env_name, snapshot_name):
     fuel_ip = request.config.getoption("--fuel-ip")
     if not fuel_ip:
         fuel_ip = DevopsClient.get_admin_node_ip(env_name=env_name)
-        revert_snapshot(env_name, snapshot_name)
-        setattr(request.node, 'reverted', True)
+        revert_snapshot(request, env_name, snapshot_name)
+        setattr(request.node, 'do_revert', False)
     if not fuel_ip:
         fuel_ip = SERVER_ADDRESS
     return fuel_ip
@@ -108,8 +119,8 @@ def clean_os(os_conn):
 
 @pytest.yield_fixture(scope="function")
 def setup(request, env_name, snapshot_name, env, os_conn):
-    if not getattr(request.node, 'reverted', False) and env_name:
-        revert_snapshot(env_name, snapshot_name)
+    if env_name:
+        revert_snapshot(request, env_name, snapshot_name)
     yield
     if not env_name:
         clear_l3_ban(env, os_conn)
@@ -128,7 +139,7 @@ def tshark():
 @pytest.fixture
 def check_ha_env(env):
     """Check that deployment type is HA"""
-    if not env.is_ha:
+    if not env.is_ha or len(env.get_nodes_by_role('controller')) < 3:
         pytest.skip('requires HA cluster')
 
 
