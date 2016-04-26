c960133523c3feb5a13d16667bf298e90c66de92
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-04-26 10:22:54 +0300

    Add Nova test 'Migrate instances from host'
    This test checks migration all instances of the specified host to other
    available hosts
    
    Change-Id: Iec60441586f492913a8f8fcafadeb652987f34a1

--- mos_tests/nova/conftest.py
+++ mos_tests/nova/conftest.py
@@ -45,13 +45,13 @@ def security_group(os_conn, request):
         os_conn.delete_security_group(name=sec_group.name)
 
 
-@pytest.yield_fixture
+@pytest.yield_fixture(params=[{'count': 2}], ids=['2 vms'])
 def instances(request, os_conn, security_group, keypair, network):
-    """2 instances on one compute node at one network"""
+    """Some instances on one compute node at one network"""
     zone = os_conn.nova.availability_zones.find(zoneName="nova")
     compute_host = zone.hosts.keys()[0]
     instances = []
-    for i in range(2):
+    for i in range(request.param['count']):
         instance = os_conn.create_server(
             name='server%02d' % i,
             availability_zone='{}:{}'.format(zone.zoneName, compute_host),
