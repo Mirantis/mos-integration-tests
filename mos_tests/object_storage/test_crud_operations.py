b92c8cde83d91a5910ce54873e5c6409c3ce6665
Alexander Koryagin <akoryag@mera.ru>
2016-05-19 06:48:05 +0000

    ObjStorage: Change Swift client to OpenStack client. Add tox.
    QA-2386 - Automation of CRUD Operations Tests (Object Storage)
    
    Change-Id: Iceb34e155c094b5867c4d85f715161da1225ded5

--- mos_tests/object_storage/test_crud_operations.py
+++ mos_tests/object_storage/test_crud_operations.py
@@ -23,25 +23,10 @@ from mos_tests.neutron.python_tests.base import TestBase
 logger = logging.getLogger(__name__)
 
 
-class Helpers(object):
-
-    @staticmethod
-    def wa_for_1543135(env):
-        """WA for: Wrong OS_AUTH_URL makes keystone operations fail
-        https://bugs.launchpad.net/mos/+bug/1543135
-        Changes OS_AUTH_URL from ":5000/" --> ":5000/v2.0" in /root/openrc
-        """
-        logger.warning('Applying WA for bug #1543135')
-        cmd = r"sed -i 's#:5000/\x27#:5000/v2.0\x27#g' /root/openrc"
-        for controller in env.get_nodes_by_role('controller'):
-            with controller.ssh() as remote:
-                remote.check_call(cmd)
-
-
 @pytest.mark.undestructive
 @pytest.mark.check_env_('is_ceph_enabled')
 @pytest.mark.check_env_('is_radosgw_enabled')
-class TestObjectStorageS3CMD(TestBase, Helpers):
+class TestObjectStorageS3CMD(TestBase):
     """Object Storage - CRUD Operations Tests
     """
     f_size = 100       # MB
@@ -182,22 +167,17 @@ class TestObjectStorageS3CMD(TestBase, Helpers):
 @pytest.mark.undestructive
 @pytest.mark.check_env_('is_ceph_enabled')
 @pytest.mark.check_env_('is_radosgw_enabled')
-class TestObjectStorageSWIFT(TestBase, Helpers):
+class TestObjectStorageSWIFT(TestBase):
     """Object Storage - CRUD Operations Tests
     """
     f_size = 100         # MB
-    f_size_b = 5432      # MB
+    f_sizebig = 5432      # MB
     segment_size = 1024  # MB
-
-    # Set "autouse=True" only for test-automation purposes
-    @classmethod
-    @pytest.fixture(scope='class', autouse=False)  # True to enable
-    def apply_wa(cls, env):
-        cls.wa_for_1543135(env)
+    f_size_bytes = f_size * 1024 * 1024
 
     @pytest.mark.testrail_id('842481')
     def test_object_storage_swift_container_create(
-            self, swift_client, swift_container):
+            self, os_swift_client, swift_container):
         """Create container in Object Storage (RadosGW)
 
         Actions:
@@ -206,13 +186,12 @@ class TestObjectStorageSWIFT(TestBase, Helpers):
         3. Delete container.
         """
         bname = swift_container
-        list_out = swift_client.list()
-        assert bname in ''.join(list_out)
-        swift_client.delete(bname)
+        list_out = os_swift_client.container_list()
+        assert bname in [x['Name'] for x in list_out]
 
     @pytest.mark.testrail_id('842482')
     def test_object_storage_swift_container_delete(
-            self, swift_client, swift_container):
+            self, os_swift_client, swift_container):
         """Delete container from Object Storage (RadosGW)
 
         Actions:
@@ -221,15 +200,14 @@ class TestObjectStorageSWIFT(TestBase, Helpers):
         3. Show containers list and check that container doesn't presents.
         """
         bname = swift_container
-        del_out = swift_client.delete(bname)
-        list_out = swift_client.list()
-        assert bname in del_out
-        assert bname not in ''.join(list_out)
+        os_swift_client.container_delete(bname)
+        list_out = os_swift_client.container_list()
+        assert bname not in str(list_out)
 
     @pytest.mark.testrail_id('842485')
     @pytest.mark.parametrize('create_file_on_node', [f_size], indirect=True)
     def test_object_storage_swift_obj_upload(
-            self, swift_client, swift_container, create_file_on_node):
+            self, os_swift_client, swift_container, create_file_on_node):
         """Upload object to Object Storage (RadosGW)
 
         Actions:
@@ -241,16 +219,16 @@ class TestObjectStorageSWIFT(TestBase, Helpers):
         """
         f_path, f_name = create_file_on_node
         bname = swift_container
-        upl_out = swift_client.upload(bname, f_path)
-        ls_in_out = swift_client.list(bname)
-        swift_client.delete(bname)
-        assert upl_out == f_path[1:]
-        assert f_path[1:] in ''.join(ls_in_out)
+        upl_out = os_swift_client.object_create(bname, f_path)
+        ls_in_out = os_swift_client.object_list(bname)
+        assert any(f_path == x['object'] for x in upl_out)
+        assert any(f_path == x['Name'] for x in ls_in_out)
+        assert any(self.f_size_bytes == x['Bytes'] for x in ls_in_out)
 
     @pytest.mark.testrail_id('842483')
     @pytest.mark.parametrize('create_file_on_node', [f_size], indirect=True)
     def test_object_storage_swift_obj_delete(
-            self, swift_client, swift_container, create_file_on_node):
+            self, os_swift_client, swift_container, create_file_on_node):
         """Delete object from Object Storage (RadosGW)
 
         Actions:
@@ -263,17 +241,15 @@ class TestObjectStorageSWIFT(TestBase, Helpers):
         """
         f_path, f_name = create_file_on_node
         bname = swift_container
-        swift_client.upload(bname, f_path)
-        del_out = swift_client.delete(bname, f_path[1:])
-        ls_in_out = swift_client.list(bname)
-        swift_client.delete(bname)
-        assert f_path[1:] in del_out
-        assert bname not in ''.join(ls_in_out)
+        os_swift_client.object_create(bname, f_path)
+        os_swift_client.object_delete(bname, f_path)
+        ls_in_out = os_swift_client.object_list(bname)
+        assert bname not in str(ls_in_out)
 
     @pytest.mark.testrail_id('842484')
-    @pytest.mark.parametrize('create_file_on_node', [f_size_b], indirect=True)
+    @pytest.mark.parametrize('create_file_on_node', [f_sizebig], indirect=True)
     def test_object_storage_swift_obj_upload_big(
-            self, swift_client, swift_container, create_file_on_node):
+            self, os_swift_client, swift_container, create_file_on_node):
         """Upload big object to Object Storage (RadosGW)
 
         Actions:
@@ -284,21 +260,12 @@ class TestObjectStorageSWIFT(TestBase, Helpers):
         5. Check that size of container is the same as it was created.
         6. Check expected and actual chunk slice number.
         5. Delete container.
+
+        BUG: https://bugs.launchpad.net/mos/+bug/1583033
         """
-        file_size_b = str(self.f_size_b * 1024 * 1024)  # 3365928960
         f_path, f_name = create_file_on_node
         bname = swift_container
-        swift_client.upload(bname, f_path,
-                            option='-S %sM' % self.segment_size)
-        ls_in_out = swift_client.list(bname + '_segments')
-        swift_client.delete(bname)
-        swift_client.delete(bname + '_segments')
-        # check file present in container
-        assert f_path[1:] in ''.join(ls_in_out)
-        # check file size is the same on node and on swift
-        assert ls_in_out[-1].strip() == file_size_b
-        # check num of slices
-        exp_chunk_slices = math.ceil(
-            float(self.f_size_b) / float(self.segment_size))
-        actual_slices = len(ls_in_out[:-1])
-        assert exp_chunk_slices == actual_slices
+        os_swift_client.object_create(bname, f_path)   # not working
+        ls_in_out = os_swift_client.object_list(bname)
+        assert any(f_path == x['Name'] for x in ls_in_out)
+        assert any(self.f_size_bytes == x['Bytes'] for x in ls_in_out)
