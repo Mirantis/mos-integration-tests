77a775ab2b7db2a5147bb44506eaba3c05448a25
Evgeny Sikachev <esikachev@mirantis.com>
2016-05-30 13:07:27 +0300

    [CPU Pinning] Check VMs with old flavor
    Check vm with 1 numa and vm without numa and check
    connectivity
    
    Change-Id: Ia2def1a5661ee3777ec46a8ea82a8df000fad3f4

--- mos_tests/nfv/test_cpu_pinning.py
+++ mos_tests/nfv/test_cpu_pinning.py
@@ -12,6 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import csv
 import logging
 
 import pytest
@@ -23,6 +24,29 @@ from mos_tests.nfv.conftest import get_cpu_distribition_per_numa_node
 logger = logging.getLogger(__name__)
 
 
+@pytest.yield_fixture
+def aggregate_n(os_conn):
+    numa_computes = []
+    for compute in os_conn.env.get_nodes_by_role('compute'):
+        with compute.ssh() as remote:
+            res = remote.check_call("lscpu -p=cpu,node | "
+                                    "grep -v '#'")["stdout"]
+        reader = csv.reader(res)
+        numas = {int(numa[1]) for numa in reader}
+        if len(numas) > 1:
+            numa_computes.append(compute)
+    if len(numa_computes) < 2:
+        pytest.skip("Insufficient count of compute with Numa Nodes")
+    aggr = os_conn.nova.aggregates.create('performance_n', 'nova')
+    os_conn.nova.aggregates.set_metadata(aggr, {'pinned': 'false'})
+    for host in numa_computes:
+        os_conn.nova.aggregates.add_host(aggr, host.data['fqdn'])
+    yield aggr
+    for host in numa_computes:
+        os_conn.nova.aggregates.remove_host(aggr, host.data['fqdn'])
+    os_conn.nova.aggregates.delete(aggr)
+
+
 @pytest.mark.check_env_('is_vlan')
 class TestCpuPinningOneNuma(TestBaseNFV):
 
@@ -31,7 +55,10 @@ class TestCpuPinningOneNuma(TestBaseNFV):
          'params': {'ram': 2048, 'vcpus': 2, 'disk': 20},
          'keys': {'aggregate_instance_extra_specs:pinned': 'true',
                   'hw:cpu_policy': 'dedicated',
-                  'hw:numa_nodes': 1}}]
+                  'hw:numa_nodes': 1}},
+        {'name': 'm1.small.old',
+         'params': {'ram': 2048, 'vcpus': 2, 'disk': 20},
+         'keys': {'aggregate_instance_extra_specs:pinned': 'false'}}]
 
     @pytest.mark.undestructive
     @pytest.mark.testrail_id('838318')
@@ -115,5 +142,59 @@ class TestCpuPinningTwoNumas(TestBaseNFV):
             host = getattr(vm, "OS-EXT-SRV-ATTR:host")
             assert host in hosts
             self.check_cpu_for_vm(os_conn, vm, 2, cpus[host])
+
+        network_checks.check_vm_connectivity(env, os_conn)
+
+
+@pytest.mark.check_env_('is_vlan')
+class TestCpuPinningOldFlavor(TestBaseNFV):
+
+    flavors_to_create = [
+        {'name': 'm1.small.perfomance',
+         'params': {'ram': 2048, 'vcpus': 2, 'disk': 20},
+         'keys': {'aggregate_instance_extra_specs:pinned': 'true',
+                  'hw:cpu_policy': 'dedicated',
+                  'hw:numa_nodes': 1}},
+        {'name': 'm1.small.old',
+         'params': {'ram': 2048, 'vcpus': 2, 'disk': 20},
+         'keys': {'aggregate_instance_extra_specs:pinned': 'false'}}]
+
+    @pytest.mark.undestructive
+    @pytest.mark.testrail_id('838324')
+    def test_cpu_pinning_old_flavor(
+            self, env, os_conn, networks, flavors, security_group,
+            aggregate, aggregate_n):
+        """This test checks that cpu pinning executed successfully for
+        instances created on computes with 1 NUMA
+        Steps:
+            1. Create net1 with subnet, net2 with subnet and router1 with
+            interfaces to both nets
+            2. Launch instances vm0 in net1 with m1.small.performance,
+            with m1.small.old vm2 on compute-2, vm3 on compute-1.
+            3. Check numa nodes for vms
+            4. Check parameter in /etc/defaults/grub
+            5. Check vms connectivity
+        """
+        hosts = aggregate.hosts
+        hosts_n = aggregate_n.hosts
+        vms = []
+        flavors_for_instances = [flavors[0], flavors[1], flavors[0]]
+        network_for_instances = [networks[0], networks[1], networks[0]]
+        hosts_for_instances = [hosts[0], hosts[1], hosts_n[0]]
+        cpus = get_cpu_distribition_per_numa_node(env)
+
+        for i in range(2):
+            vms.append(os_conn.create_server(
+                name='vm{}'.format(i),
+                flavor=flavors_for_instances[i].id,
+                nics=[{'net-id': network_for_instances[i]}],
+                availability_zone='nova:{}'.format(hosts_for_instances[i]),
+                security_groups=[security_group.id]))
+
+        for vm in vms:
+            host = getattr(vm, "OS-EXT-SRV-ATTR:host")
+            assert host in hosts
+            if vm.name != 'vm1':
+                self.check_cpu_for_vm(os_conn, vm, 1, cpus[host])
 
         network_checks.check_vm_connectivity(env, os_conn)
