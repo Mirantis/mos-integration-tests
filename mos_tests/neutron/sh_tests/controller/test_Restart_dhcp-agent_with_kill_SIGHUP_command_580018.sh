8e5906f10efa997173eb426ea0dabd1ff374d0b8
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-08-03 12:08:09 +0300

    Remove `screen` from sh neutron tests
    QA-3569
    
    Change-Id: Id57f8b88a4fa50df4da0d39ceaa514c014df54f1

--- mos_tests/neutron/sh_tests/controller/test_Restart_dhcp-agent_with_kill_SIGHUP_command_580018.sh
+++ mos_tests/neutron/sh_tests/controller/test_Restart_dhcp-agent_with_kill_SIGHUP_command_580018.sh
@@ -2,43 +2,56 @@
 
 . openrc
 
-screen -S DHCP_SIG -d -m -- sh -c 'tailf /var/log/neutron/dhcp-agent.log > log_dhcp'
+LOG_FILE=$(find /var/log/neutron -name *dhcp-agent*log)
+LOG_LENGTH=$(cat $LOG_FILE | wc -l)
+
+read_log () {
+    tail -n +$LOG_LENGTH $LOG_FILE
+}
+
 TEST_FAILED=0
+
 echo "Get a pid of a process for dhcp-agent"
-pid_before=$(ps -aux | grep -v root | grep neutron-dhcp-agent | awk '{print $2}')
-echo "Pid for dhcp-agent process is "$pid_before
+PID_BEFORE=$(ps -aux | grep -v root | grep neutron-dhcp-agent | awk '{print $2}')
+echo "Pid for dhcp-agent process is "$PID_BEFORE
+
 echo "Kill a process with HUP "
-kill -SIGHUP $pid_before
+kill -SIGHUP $PID_BEFORE
 sleep 10
+
 echo "Check health of dhcp-agent"
 neutron agent-list | grep 'DHCP agent'
+
 echo "Check status of a process after HUP restar"
-pid_after=$(ps -aux | grep neutron-dhcp-agent | grep -v root | awk '{print $2}')
-echo "PID after ="$pid_after
-if [ $pid_before = $pid_after ]; then
+PID_AFTER=$(ps -aux | grep neutron-dhcp-agent | grep -v root | awk '{print $2}')
+echo "PID after ="$PID_AFTER
+if [ $PID_BEFORE = $PID_AFTER ]; then
     echo "PIDs are equal"
 else
     echo "ERROR: pids are not equal"
     TEST_FAILED=1
 fi
-screen -X -S DHCP_SIG kill
+
 echo "Try to find ERROR in dhcp log"
-cat log_dhcp | grep ERROR
+read_log | grep ERROR
+
 echo "Try to find TRACE in dhcp log"
-cat log_dhcp | grep TRACE
+read_log | grep TRACE
+
 echo "Try to find SIGHUP command"
-cat log_dhcp | grep SIGHUP
-NUMBER=$(cat log_dhcp | grep SIGHUP | wc -l)
-echo "Find SIGHUP "$NUMBER" times"
-if [ $NUMBER -ne 1 ]; then
+read_log | grep SIGHUP
+
+SIGHUP_COUNT=$(read_log | grep SIGHUP | wc -l)
+echo "Find SIGHUP "$SIGHUP_COUNT" times"
+if [ $SIGHUP_COUNT -ne 1 ]; then
     echo "ERROR: There was no SIGHUP command or it was sent more than one time"
     TEST_FAILED=1
 fi
-rm -r log_dhcp
 
 if [ $TEST_FAILED == 0 ]; then
     echo "Test "$TEST_NAME" PASSED"
 else
+    read_log
     echo "Test "$TEST_NAME" FAILED"
     exit 1
 fi
--- mos_tests/neutron/sh_tests/controller/test_Restart_ovs-agent_with_kill_SIGHUP_command_580020.sh
+++ mos_tests/neutron/sh_tests/controller/test_Restart_ovs-agent_with_kill_SIGHUP_command_580020.sh
@@ -2,16 +2,26 @@
 
 . openrc
 
-screen -S OVS_SIG -d -m -- sh -c 'tailf /var/log/neutron/openvswitch-agent.log > log_ovs'
+LOG_FILE=$(find /var/log/neutron -name *openvswitch-agent*log)
+LOG_LENGTH=$(cat $LOG_FILE | wc -l)
+
+read_log () {
+    tail -n +$LOG_LENGTH $LOG_FILE
+}
+
 TEST_FAILED=0
+
 echo "Get a pid of a process for ovs-agent"
 pid_before=$(ps -aux | grep -v root | grep neutron-openvswitch-agent | awk '{print $2}')
 echo "Pid for ovs-agent process is "$pid_before
+
 echo "Kill a process with HUP "
 kill -SIGHUP $pid_before
 sleep 10
+
 echo "Check health ovs-agent"
 neutron agent-list | grep 'Open vSwitch agent'
+
 echo "Check status of a process after HUP restar"
 pid_after=$(ps -aux | grep  neutron-openvswitch-agent | grep -v root | awk '{print $2}')
 echo "PID after ="$pid_after
@@ -21,20 +31,21 @@ else
     echo "ERROR: pids are not equal"
     TEST_FAILED=1
 fi
-screen -X -S OVS_SIG kill
+
 echo "Try to find ERROR in ovs log"
-cat log_ovs | grep ERROR
+read_log | grep ERROR
+
 echo "Try to find TRACE in ovs log"
-cat log_ovs | grep TRACE
+read_log | grep TRACE
+
 echo "Try to find SIGHUP command"
-cat log_ovs | grep SIGHUP
-NUMBER=$(cat log_ovs | grep SIGHUP | wc -l)
+read_log | grep SIGHUP
+NUMBER=$(read_log | grep SIGHUP | wc -l)
 echo "Find SIGHUP "$NUMBER" times"
 if [ $NUMBER -ne 1 ]; then
     echo "ERROR: There was no SIGHUP command or it was sent more than one time"
     TEST_FAILED=1
 fi
-rm -r log_ovs
 
 if [ $TEST_FAILED == 0 ]; then
     echo "Test "$TEST_NAME" PASSED"
